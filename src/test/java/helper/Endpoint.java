8653d40d4197c7a4d86bcdbb56026a5c62c32c64
galihnkk <galihnkk@users.noreply.github.com>
2024-10-18 11:35:17 +0000

    deploy: 0f2328e849ab8d5cca8dff1af9aaf4558a51d377

--- src/test/java/helper/Endpoint.java
+++
@@ -1,9 +0,0 @@
-package helper;
-
-public class Endpoint {
-    public static final String baseURL = "https://dummyapi.io/data/v1/";
-    public static final String GET_USER = baseURL + "user";
-    public static final String POST_USER = baseURL + "user/create";
-    public static final String GET_TAGS = baseURL + "tag";
-
-}
